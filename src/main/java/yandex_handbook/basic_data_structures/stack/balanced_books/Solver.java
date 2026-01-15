d464155982e93bb7e65ca63e0b2e0e1dad1fc88a
hairlessing <kamagin.andrey@gmail.com>
2026-01-15 20:27:49 +0500

    push

---
+++ src/main/java/yandex_handbook/basic_data_structures/stack/balanced_books/Solver.java
@@ -0,0 +1,97 @@
+package yandex_handbook.basic_data_structures.stack.balanced_books;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Stack;
+
+public class Solver {
+
+    private static final FastScanner FS = new FastScanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        int numberOfElements = FS.nextInt();
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+
+        int maxStackLength = 0;
+
+        for  (int i = 0; i < numberOfElements; i++) {
+            int number = FS.nextInt();
+
+            if (isOddNumber(number)) {
+                stack.push(i);
+
+            } else {
+                stack.pop();
+
+                if (stack.empty()) {
+                    stack.push(i);
+
+                } else {
+                    maxStackLength = Math.max(maxStackLength, i - stack.peek());
+                }
+            }
+        }
+
+        System.out.print(maxStackLength);
+    }
+
+    private static boolean isOddNumber(int number) {
+        return number % 2 != 0;
+    }
+
+    static class FastScanner {
+
+        private final InputStream in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0;
+        private int len = 0;
+
+        FastScanner(InputStream is) {
+            this.in = is;
+        }
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+            while ((c = read()) <= ' ') {
+                if (c == -1) return -1;
+            }
+            int sign = 1;
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+            int val = c - '0';
+            while ((c = read()) > ' ') {
+                val = val * 10 + (c - '0');
+            }
+            return val * sign;
+        }
+
+        String nextLine() throws IOException {
+            StringBuilder sb = new StringBuilder();
+            int c;
+
+            while ((c = read()) == '\n' || c == '\r') {
+            }
+
+            if (c == -1) return null;
+
+            do {
+                sb.append((char) c);
+            } while ((c = read()) != -1 && c != '\n' && c != '\r');
+
+            return sb.toString();
+        }
+    }
+}
